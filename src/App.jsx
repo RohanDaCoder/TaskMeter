584fa7c3f360bb745b2276dddb98dccae57a836c
AL Zami Rohan <102975537+RohanDaCoder@users.noreply.github.com>
2025-10-31 12:30:09 +0000

    Intial Commit 2.0 ;)

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-white font-bold font-mono text-center text-6xl pt-5">
+        TaskMeter
+      </h1>
+
+      <div className="pt-[5px] rounded border-white"> 
+        <p>Tasks</p>
+        </div> 
+    </>
+  );
+}
+
+export default App;
