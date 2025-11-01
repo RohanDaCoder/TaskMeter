d71b042d79c5bc38e6b02942ab52c398cbc82d90
AL Zami Rohan <102975537+RohanDaCoder@users.noreply.github.com>
2025-11-01 10:36:20 +0000

    style: use lucide icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
+import { Folder } from 'lucide-react';
+
 function App() {
   return (
     <>
       <h1 className="pt-5 text-center font-mono text-6xl font-bold text-white">
-        TaskMeter
+        <span className="inline-flex items-center justify-center gap-3">
+          TaskMeter <Folder size={60} />
+        </span>
       </h1>
 
       <div className="mx-100 my-10 rounded-md border-white bg-white p-40 text-center text-2xl font-bold shadow-md shadow-black transition-colors delay-50 hover:bg-slate-400">
